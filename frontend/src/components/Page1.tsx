0d26148827ca2422a1b2b8b1d8e0138dcebbfae8
Vaishnavi Rajput <141741075+seduloushaze@users.noreply.github.com>
2024-08-16 16:16:34 +0530

    creating repo

--- frontend/src/components/Page1.tsx
+++ frontend/src/components/Page1.tsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import LanguageSetup from './LanguageSetup'
-
+import LottieView from 'lottie-react-native';
 
 export default function Page1() {
   return (
     <View>
+       <LottieView source={require('../assets/final-lottie.json')} autoPlay loop />;
         <LanguageSetup />
     </View>
   )
